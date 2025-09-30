4ab2256faf151e33ba9a3b8bfbc8f1f94f245c2c
cheezecakee <anabel.pmoraes@gmail.com>
2025-09-29 23:57:12 -0300

    feat: define Formatter interface and PlainTextFormatter implementation

--- formats.go
+++ formats.go
@@ -1,1 +1,18 @@
 package logr
+
+import (
+	"fmt"
+	"time"
+)
+
+const TimeFormat = time.RFC3339
+
+type Formatter interface {
+	Format(entry LogEntry) string
+}
+
+type PlainTextFormatter struct{}
+
+func (f *PlainTextFormatter) Format(entry LogEntry) string {
+	return fmt.Sprintf("[%s] [%s] [%v] %s", entry.Level, entry.Layer, entry.Timestamp.Format(TimeFormat), entry.Message)
+}
