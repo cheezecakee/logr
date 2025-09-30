971e8310383ed56ee5faed1af3074e9180348e00
cheezecakee <anabel.pmoraes@gmail.com>
2025-09-30 01:04:32 -0300

    feat: implement Metadata struct with Add/Get methods

--- metadata.go
+++ metadata.go
@@ -1,1 +1,23 @@
 package logr
+
+type Metadata struct {
+	data map[string]any
+}
+
+func NewMetadata() *Metadata {
+	return &Metadata{
+		data: make(map[string]any),
+	}
+}
+
+func (m *Metadata) Add(key string, value any) {
+	m.data[key] = value
+}
+
+func (m *Metadata) Get(key string) (any, bool) {
+	value, ok := m.data[key]
+	if ok {
+		return value, true
+	}
+	return nil, false
+}
