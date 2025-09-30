843d4ab777f82df654d8b9155d6cd6e337116b97
cheezecakee <anabel.pmoraes@gmail.com>
2025-09-29 23:57:12 -0300

    feat: implement LogEntry struct with NewEntry constructor

--- entry.go
+++ entry.go
@@ -1,2 +1,20 @@
 // Package logr
 package logr
+
+import "time"
+
+type LogEntry struct {
+	Level     Level
+	Layer     Layer
+	Message   string
+	Timestamp time.Time
+}
+
+func NewEntry(level Level, layer Layer, msg string) *LogEntry {
+	return &LogEntry{
+		Level:     level,
+		Layer:     layer,
+		Message:   msg,
+		Timestamp: time.Now(),
+	}
+}
