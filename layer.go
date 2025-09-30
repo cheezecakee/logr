2d786b08cf036ae41e4397341da8b7102b753885
cheezecakee <anabel.pmoraes@gmail.com>
2025-09-29 23:57:12 -0300

    feat: define Layer type, constants, String() method, and RegisterLayer()

--- layer.go
+++ layer.go
@@ -1,1 +1,19 @@
 package logr
+
+import "strings"
+
+type Layer string
+
+const (
+	LayerHTTP Layer = "HTTP"
+	LayerDB   Layer = "DB"
+	LayerCORE Layer = "CORE"
+)
+
+func (l Layer) String() string {
+	return string(l)
+}
+
+func RegisterLayer(name string) Layer {
+	return Layer(strings.ToUpper(name))
+}
