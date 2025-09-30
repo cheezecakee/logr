543eae154c2bebcfead2f57e6fd2552cd5809ca4
cheezecakee <anabel.pmoraes@gmail.com>
2025-09-30 01:04:28 -0300

    feat: add metadata support to LogEntry

--- entry.go
+++ entry.go
@@ -8,13 +8,28 @@ type LogEntry struct {
 	Layer     Layer
 	Message   string
 	Timestamp time.Time
+	Metadata  *Metadata
 }
 
-func NewEntry(level Level, layer Layer, msg string) *LogEntry {
+func NewEntry(level Level, layer Layer, msg string, meta ...Metadata) *LogEntry {
+	var metadata *Metadata
+	if len(meta) > 0 {
+		metadata = &meta[0]
+	} else {
+		metadata = NewMetadata()
+	}
 	return &LogEntry{
 		Level:     level,
 		Layer:     layer,
 		Message:   msg,
 		Timestamp: time.Now(),
+		Metadata:  metadata,
 	}
+}
+
+func (l *LogEntry) AddMetadata(key string, value any) {
+	if l.Metadata == nil {
+		l.Metadata = NewMetadata()
+	}
+	l.Metadata.Add(key, value)
 }
