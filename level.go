86b71cf804652e61134d078ee09f94c206045be6
cheezecakee <anabel.pmoraes@gmail.com>
2025-09-29 23:57:12 -0300

    feat: define Level type, constants, and String() method

--- level.go
+++ level.go
@@ -1,1 +1,28 @@
 package logr
+
+type Level int
+
+const (
+	LevelInfo Level = iota
+	LevelError
+	LevelDebug
+	LevelWarn
+	LevelTest
+)
+
+func (l Level) String() string {
+	switch l {
+	case LevelInfo:
+		return "INFO"
+	case LevelError:
+		return "ERROR"
+	case LevelDebug:
+		return "DEBUG"
+	case LevelWarn:
+		return "WARN"
+	case LevelTest:
+		return "TEST"
+	default:
+		return "UNKNOWN"
+	}
+}
